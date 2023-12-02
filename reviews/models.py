7a4aaf0adcb1b56ef594da6b9f4557ad0f681552
Adnan <adbak.dev@gmail.com>
2023-12-02 21:15:53 +0100

    forms forms forms

--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
 class Review(models.Model):
     user_name = models.CharField(max_length=100)
     review_text = models.TextField()
-    rating = models.IntegerField()
+    # maximum rate for rating is 5
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
 
     def __str__(self):
         return f"{self.user_name} rated {self.rating} stars"
